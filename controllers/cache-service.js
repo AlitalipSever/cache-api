fc8c66488181da72cd10b1dbee1151f9d9bb7c12
AlitalipSever <alitalipsever@gmail.com>
2022-02-06 18:36:10 +0100

    number of entries allowed logic added
    complex logic not time for explanation  :|

--- controllers/cache-service.js
+++ controllers/cache-service.js
@@ -1,6 +1,6 @@
 const CacheData = require("../models/CacheData")
-const {json} = require("express");
 
+const DOCS_NUMBER_LIMIT = 5
 
 const cacheDataGivenKey = async (req, res) => {
     const dataId = req.body.id
@@ -18,6 +18,7 @@ const cacheDataGivenKey = async (req, res) => {
         const dataObj = {
             data: data
         }
+
         const newCacheData = new CacheData(dataObj)
         const savedCacheData = await newCacheData.save()
         res.status(201).json(savedCacheData)            //TODO send only data string
@@ -39,39 +40,111 @@ const createAndUpdateKey = async (req, res) => {
       const dataId = req.body.id
       const data = req.body.data
 
-      const updatedData = await CacheData.findByIdAndUpdate(
-          dataId,
-          //TODO Add id null check
-          {
-              $set: {data: data}
-          },
-          {new:true}
-      );
-      res.status(200).json(updatedData)             //TODO fix error sending
+      if(dataId && data) {
+
+          CacheData.count({}, async function (err, count) {           // get number counts all docs
+              console.log("Number of docs: ", count);
+              if (count > DOCS_NUMBER_LIMIT) {                                                             // if the specified number exceeds
+                  CacheData.findOne({}, [], {$orderby: {'updatedAt': -1}}, async function (err, lastDoc) {
+                      console.log(lastDoc.id);                                    // get last updated doc
+
+                      const updatedData = await CacheData.findByIdAndUpdate(
+                          lastDoc.id,                                               // update to over last updated doc
+                          //TODO Add id null check
+                          {
+                              $set: {data: data}
+                          },
+                          {new: true}
+                      );
+                      res.status(200).json(updatedData)             //TODO fix error sending
+                  });
+              } else {
+                  CacheData.count({}, async function (err, count) {  // get number counts all docs
+                      console.log("Number of docs: ", count);
+                      if (count === 0) {
+                          const data = req.body.data
+                          const dataObj = {
+                              data: data,
+                          }
+                          const newCacheData = new CacheData(dataObj)
+                          const savedCacheData = await newCacheData.save()
+                          res.status(201).json(savedCacheData)                   //TODO send only data string
+                      } else {
+                          const updatedData = await CacheData.findByIdAndUpdate(              // if the specified number NOT exceeds
+                              dataId,                                                      // update normal way
+                              //TODO Add id null check
+                              {
+                                  $set: {data: data}
+                              },
+                              {new: true}
+                          );
+                          res.status(200).json(updatedData)                 //TODO fix error sending
+                      }
+
+                  })
+              }
+          });
+      } else {
+          CacheData.count({}, async function(err, count){
+              console.log( "Number of docs: ", count );
+              if(count>DOCS_NUMBER_LIMIT){                              // if the specified number exceeds
+                  CacheData.findOne({}, [], { $orderby : { 'updatedAt' : -1 } }, async function(err, lastDoc) {
+                      console.log( lastDoc.id );
+
+                      const updatedData = await CacheData.findByIdAndUpdate(
+                          lastDoc.id,                                   // update last doc
+                          //TODO Add id null check
+                          {
+                              $set: {data: data}
+                          },
+                          {new:true}
+                      );
+                      res.status(200).json(updatedData)             //TODO fix error sending
+                  });
+              }else {
+                  const data = req.body.data
+                  const dataObj = {                             // NOT exceeds get data and write CREATE new doc with NEW Id
+                      data: data,
+                  }
+                  const newCacheData = new CacheData(dataObj)
+                  const savedCacheData = await newCacheData.save()
+                  res.status(201).json(savedCacheData)                   //TODO send only data string
+              }
+          })
+      }
+
   }catch (e) {
 
-      const data = req.body.data
-      const dataObj = {
-          data: data,
-      }
-      const newCacheData = new CacheData(dataObj)
-      const savedCacheData = await newCacheData.save()
-      res.status(201).json(savedCacheData)            //TODO send only data string
   }
 }
 
 const removeGivenKey = async (req, res) => {
     try{
-        await CacheData.findByIdAndDelete(req.body.id)
+        await CacheData.findByIdAndDelete(req.body.id)      //TODO add body check
         res.status(200).json("cache key has been deleted")
     }catch (e) {
         res.status(500).json(e)
     }
 }
 
+const removeAllKeys = async (req, res) => {
+  try {
+      await CacheData.deleteMany({})      //TODO add body check
+      res.status(200).json("cache keys has been deleted")
+  }catch (e) {
+      res.status(500).json(e)
+  }
+}
 
 
 
 
 
-module.exports = {cacheDataGivenKey, allStoredKeys, createAndUpdateKey, removeGivenKey}
+
+module.exports = {
+    cacheDataGivenKey,
+    allStoredKeys,
+    createAndUpdateKey,
+    removeGivenKey,
+    removeAllKeys
+}
--- routes/cache-api.js
+++ routes/cache-api.js
@@ -1,10 +1,9 @@
 const {verifyTokenAndAdmin, verifyToken} = require("../middlewares/verify-token")
-const {cacheDataGivenKey, allStoredKeys, createAndUpdateKey, removeGivenKey} = require("../controllers/cache-service")
+const {cacheDataGivenKey, allStoredKeys, createAndUpdateKey, removeGivenKey, removeAllKeys} = require("../controllers/cache-service")
 const router = require("express").Router()
 
 //CACHE DATA FOR GIVEN KEY
 router.post("/getData", verifyTokenAndAdmin, cacheDataGivenKey)
-
 
 //ALL STORED KEYS
 router.get("/all", verifyTokenAndAdmin, allStoredKeys)
@@ -16,10 +15,6 @@ router.put("/createAndUpdate", verifyTokenAndAdmin, createAndUpdateKey)
 router.delete("/removeKey", verifyTokenAndAdmin, removeGivenKey)
 
 //REMOVE ALL KEYS
-router.delete("/all", verifyTokenAndAdmin, async (req, res) => {
-    console.log("REMOVE ALL KEYS");
-    const id = req.params.id
-    console.log(id);
-})
+router.delete("/all", verifyTokenAndAdmin, removeAllKeys)
 
 module.exports = router
